025a7e9685b6a3279aabde0f8a40c9e95ccdb753
damithsenanayake <dracusds123@gmail.com>
2020-04-08 09:29:05 +1000

    version 0.1.1 release ready version for testing.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 import setuptools
 
-setuptools.setup(name = "song-damith-senanayake", version = "0.1.1", author = "Damith Senanayake", author_email = "dracusds123@gmail.com", description = "A dimensionality reduction method that retains a parametric model"
+setuptools.setup(name = "song_vis", version = "0.1.1", author = "Damith Senanayake", author_email = "dracusds123@gmail.com", description = "A dimensionality reduction method that retains a parametric model"
                  , packages = setuptools.find_packages())
