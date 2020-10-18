48a6a9dcc5360268b0c1d21aef01a52c3697a388
damithsenanayake <dracusds123@gmail.com>
2020-10-18 11:47:57 +1100

    sparse memory issue fixed

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ import setuptools
 
 setup(
     name = "song_vis",
-    version = "1.1.0",
+    version = "1.1.1",
     author = "Damith Senanayake",
     author_email = "dracusds123@gmail.com",
     description = "A dimensionality reduction method that retains a parametric model",
     packages = setuptools.find_packages(),
-    download_url = 'https://github.com/damithsenanayake/SONG/archive/v1.0.2.tar.gz'
+    download_url = 'https://github.com/damithsenanayake/SONG/archive/v1.1.1.tar.gz'
 )
