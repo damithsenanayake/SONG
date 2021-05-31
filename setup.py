bd735930fb40fe7ab69e9e5f9568f6699902583d
Damith Senanayake <dracusds123@gmail.com>
2021-05-31 10:16:46 +1000

    transform now have PC space transformation caveats: * when transform is called, the input and output is uniform. * for non-uniform (heterogeneous) distributions, fit_transform must be called.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setup(
     name = "song_vis",
-    version = "1.2.0.14",
+    version = "1.3.0.0",
     author = "Damith Senanayake",
     author_email = "dracusds123@gmail.com",
     description = "A dimensionality reduction method that retains a parametric model",
